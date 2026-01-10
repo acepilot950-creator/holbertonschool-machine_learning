8b68d5f5db862ab28bb865b50f4afdad4c5837f7
acepilot950-creator <acepilot950@gmail.com>
2026-01-10 23:39:28 +0400

    initial commit

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module for plotting a scatter plot of men's height vs weight.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """
+    Plots a scatter graph of men's height versus weight.
+    """
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+
+    plt.figure(figsize=(6.4, 4.8))
+    plt.scatter(x, y, c='magenta')
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.title("Men's Height vs Weight")
+    plt.show()
