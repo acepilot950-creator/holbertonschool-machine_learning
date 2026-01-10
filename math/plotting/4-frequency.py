d2c23c0f539f2eb618ab767f5b1ada3a5e75e13d
acepilot950-creator <acepilot950@gmail.com>
2026-01-10 23:44:16 +0400

    initial commit

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module for plotting a histogram of student grades.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """
+    Plots a histogram showing the distribution of student grades.
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+
+    plt.figure(figsize=(6.4, 4.8))
+    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.show()
