0864577ef7dfe8127452fa590b3927f563d86088
acepilot950-creator <acepilot950@gmail.com>
2026-01-11 00:09:14 +0400

    initial commit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -2,21 +2,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
 
     plt.figure(figsize=(6.4, 4.8))
 
-    # Histogram: bin width = 10, black edges
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
 
-    # Labels and title
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
 
-    # Match reference axes and ticks
     plt.xlim(0, 100)
     plt.xticks(range(0, 101, 10))
 
