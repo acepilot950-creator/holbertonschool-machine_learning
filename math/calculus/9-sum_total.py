b7c8dca1d1a5b0e8fabed1fb86cf88e611cc6c55
acepilot950-creator <acepilot950@gmail.com>
2026-01-25 23:07:42 +0400

    initial commit

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Module that provides a function to calculate the sum of numbers."""
+
+
 def summation_i_squared(n):
     if type(n) is not int or n < 1:
         return None
