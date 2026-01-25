90d81d4e150be5d6db9fce68f2091261bfed6b20
acepilot950-creator <acepilot950@gmail.com>
2026-01-25 22:45:51 +0400

    initial commit

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def summation_i_squared(n):
-    sum = 0 
-    if n >= 0 and type(n) == int:
-        for i in range(1,n+1):
+    sum = 0
+    if n >= 0 and type(n) is int:
+        for i in range(1, n+1):
             sum += i ** 2
         return sum
     else:
         return None
-    
-
-
