141360a1bfb3663cff35a27d6ae820a24601660e
acepilot950-creator <acepilot950@gmail.com>
2026-03-02 00:44:08 +0400

    initial commit

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that provides the shape of a numpy.ndarray."""
+
+
+def np_shape(matrix):
+    """Return the shape of a numpy.ndarray.
+
+    Args:
+        matrix (numpy.ndarray): Input array.
+
+    Returns:
+        tuple: Shape of the array.
+    """
+    return matrix.shape
