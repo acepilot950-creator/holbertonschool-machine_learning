792c337ddf60502cd7fd99e4f2c3614a4815d4a9
acepilot950-creator <acepilot950@gmail.com>
2026-03-01 22:13:54 +0400

    initial commit

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module that provides element-wise addition of 2D matrices."""
+
+
+def add_matrices2D(mat1, mat2):
+    """Add two 2D matrices element-wise.
+
+    Args:
+        mat1 (list of lists): First matrix.
+        mat2 (list of lists): Second matrix.
+
+    Returns:
+        list of lists: New matrix with summed elements,
+        or None if matrices have different shapes.
+    """
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    return [[mat1[i][j] + mat2[i][j]
+             for j in range(len(mat1[0]))]
+            for i in range(len(mat1))]
