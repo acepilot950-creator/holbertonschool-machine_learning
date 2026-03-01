4fd61e033efc1a2aeac50a3801feda7237ee9914
acepilot950-creator <acepilot950@gmail.com>
2026-03-01 22:15:42 +0400

    initial commit

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module that provides array concatenation."""
+
+
+def cat_arrays(arr1, arr2):
+    """Concatenate two arrays.
+
+    Args:
+        arr1 (list): First list of numbers.
+        arr2 (list): Second list of numbers.
+
+    Returns:
+        list: New list containing elements of arr1 followed by arr2.
+    """
+    return arr1 + arr2
