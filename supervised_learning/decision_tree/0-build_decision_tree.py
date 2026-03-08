18adc312dbe6565ea907c655beb2f51ed4dd1242
acepilot950-creator <acepilot950@gmail.com>
2026-03-09 03:42:49 +0400

    initial commit

---
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        left_depth = self.left_child.max_depth_below()
+        right_depth = self.right_child.max_depth_below()
+        return max(left_depth, right_depth)
