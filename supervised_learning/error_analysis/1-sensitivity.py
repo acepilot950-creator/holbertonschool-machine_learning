c169698e82e83d6f295f26ca8e8427a0da9ea338
acepilot950-creator <acepilot950@gmail.com>
2026-03-09 03:20:39 +0400

    initial commit

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module that calculates the sensitivity for each class in a confusion matrix"""
+
+import numpy as np
+
+
+def sensitivity(confusion):
+    """Calculates the sensitivity for each class
+
+    Args:
+        confusion (numpy.ndarray): confusion matrix of shape (classes, classes)
+
+    Returns:
+        numpy.ndarray: sensitivity for each class
+    """
+
+    true_positives = np.diag(confusion)
+
+    actual_total = np.sum(confusion, axis=1)
+
+    sens = true_positives / actual_total
+
+    return sens
