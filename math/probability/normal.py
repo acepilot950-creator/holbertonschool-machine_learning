6d8d77f69b5789cd49b5afba0a596f75aa3436a2
acepilot950-creator <acepilot950@gmail.com>
2026-03-09 01:41:20 +0400

    initial commit

---
+++ math/probability/normal.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Normal distribution"""
+
+
+class Normal:
+    """Represents a normal distribution."""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """Initialize the distribution."""
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            self.mean = sum(data) / len(data)
+
+            variance = 0
+            for x in data:
+                variance += (x - self.mean) ** 2
+
+            variance = variance / len(data)
+
+            self.stddev = variance ** 0.5
