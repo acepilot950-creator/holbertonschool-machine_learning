6c1c8b796c306c9ae6bfe8efb07b90dd1d760f83
acepilot950-creator <acepilot950@gmail.com>
2026-03-09 01:30:36 +0400

    initial commit

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -3,16 +3,10 @@
 
 
 class Poisson:
-    """Represents a Poisson distribution"""
+    """Represents a Poisson distribution."""
 
     def __init__(self, data=None, lambtha=1.):
-        """
-        Class constructor
-
-        Args:
-            data: list of data to estimate the distribution
-            lambtha: expected number of occurrences in a given time frame
-        """
+        """Initialize Poisson distribution."""
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -20,7 +14,23 @@ class Poisson:
         else:
             if type(data) is not list:
                 raise TypeError("data must be a list")
+
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
 
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """Calculates the PMF value for k."""
+        k = int(k)
+
+        if k < 0:
+            return 0
+
+        e = 2.7182818285
+
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+
+        return (e ** (-self.lambtha) * self.lambtha ** k) / factorial
