ffc78dcfa1e621034ce33da31978a61531954dde
acepilot950-creator <acepilot950@gmail.com>
2026-03-09 03:18:27 +0400

    initial commit

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
+"""Module that contains the function to create a confusion matrix
+for classification models.
+"""
+
 import numpy as np
 
 
 def create_confusion_matrix(labels, logits):
-    """
-    creates a confusion matrix
+    """Creates a confusion matrix.
+
+    Args:
+        labels (numpy.ndarray): one-hot array of correct labels
+                                with shape (m, classes)
+        logits (numpy.ndarray): one-hot array of predicted labels
+                                with shape (m, classes)
+
+    Returns:
+        numpy.ndarray: confusion matrix of shape (classes, classes)
+        where rows represent correct labels and columns represent
+        predicted labels.
     """
 
     classes = labels.shape[1]
