76f4fdc2c76aa9c526555f5226363dc67dfb84e6
acepilot950-creator <acepilot950@gmail.com>
2026-03-09 05:27:19 +0400

    initial commit

--- supervised_learning/decision_tree/7-build_decision_tree.py
+++ supervised_learning/decision_tree/7-build_decision_tree.py
@@ -293,13 +293,12 @@ class Decision_Tree:
 
         if verbose == 1:
             print(
-                f"""  Training finished.
-        - Depth                     : {self.depth()}
-        - Number of nodes           : {self.count_nodes()}
-        - Number of leaves          : {self.count_nodes(only_leaves=True)}
-        - Accuracy on training data : {
-            self.accuracy(self.explanatory, self.target)
-        }"""
+                "Training finished.\n"
+                f"Depth : {self.depth()}\n"
+                f"Number of nodes : {self.count_nodes()}\n"
+                f"Number of leaves : {self.count_nodes(only_leaves=True)}\n"
+                f"Accuracy on training data : "
+                f"{self.accuracy(self.explanatory, self.target)}"
             )
 
     def accuracy(self, test_explanatory, test_target):
