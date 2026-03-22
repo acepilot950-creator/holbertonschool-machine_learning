fbd14ad7ab6352081f705f6d3d51d02cfd10dcc7
acepilot950-creator <acepilot950@gmail.com>
2026-03-23 03:44:39 +0400

    initial commit

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -17,7 +17,7 @@ class DeepNeuralNetwork:
 
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(layer, int) and layer > 0 for layer in layers):
+        if not all(map(lambda x: isinstance(x, int) and x > 0, layers)):
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
