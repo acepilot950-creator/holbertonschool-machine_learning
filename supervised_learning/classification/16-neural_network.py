32bce31bb64f2db0b44c7ca87768d204a33ecf82
acepilot950-creator <acepilot950@gmail.com>
2026-03-23 03:39:00 +0400

    initial commit

---
+++ supervised_learning/classification/16-neural_network.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Module that defines a deep neural network
+performing binary classification
+"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Defines a deep neural network performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """Initialize the deep neural network"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        for layer in layers:
+            if not isinstance(layer, int) or layer <= 0:
+                raise TypeError("layers must be a list of positive integers")
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for i in range(self.L):
+            layer_size = layers[i]
+            prev_size = nx if i == 0 else layers[i - 1]
+
+            self.weights["W{}".format(i + 1)] = (
+                np.random.randn(layer_size, prev_size) * np.sqrt(2 / prev_size)
+            )
+            self.weights["b{}".format(i + 1)] = np.zeros((layer_size, 1))
