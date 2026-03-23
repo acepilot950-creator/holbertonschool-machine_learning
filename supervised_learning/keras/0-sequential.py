71c7f286403ac87922d7f250b020a42163681b3c
acepilot950-creator <acepilot950@gmail.com>
2026-03-23 14:31:57 +0400

    initial commit

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+K = tf.keras
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    builds a neural network with the Keras library
+
+    nx: number of input features
+    layers: list containing the number of nodes in each layer
+    activations: list containing the activation functions for each layer
+    lambtha: L2 regularization parameter
+    keep_prob: probability that a node will be kept for dropout
+
+    returns: the keras model
+    """
+    model = K.Sequential()
+
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(
+                K.layers.Dense(
+                    layers[i],
+                    activation=activations[i],
+                    kernel_regularizer=K.regularizers.l2(lambtha),
+                    input_shape=(nx,)
+                )
+            )
+        else:
+            model.add(
+                K.layers.Dense(
+                    layers[i],
+                    activation=activations[i],
+                    kernel_regularizer=K.regularizers.l2(lambtha)
+                )
+            )
+
+        if i != len(layers) - 1:
+            model.add(K.layers.Dropout(1 - keep_prob))
+
+    return model
---
+++ supervised_learning/keras/README.md
@@ -0,0 +1,10 @@
+# Keras Neural Network Exercises
+
+This folder contains solutions to neural network exercises implemented using **TensorFlow 2 and Keras**.
+
+## Topics covered
+- Sequential models
+- Dense layers
+- Activation functions
+- L2 regularization
+- Dropout
