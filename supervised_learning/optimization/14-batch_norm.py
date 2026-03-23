c7ad887c809fe200646f30caa357dfd44e14a40a
acepilot950-creator <acepilot950@gmail.com>
2026-03-23 17:46:50 +0400

    initial commit

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -1,26 +1,40 @@
 #!/usr/bin/env python3
-"""Module that creates a batch normalization layer in TensorFlow"""
+"""Module that creates a batch normalization layer in TensorFlow."""
 
 import tensorflow as tf
 
 
 def create_batch_norm_layer(prev, n, activation):
-    """
-    Creates a batch normalization layer for a neural network
-    """
+    """Create a batch normalization layer for a neural network.
 
+    Args:
+        prev: Activated output of the previous layer.
+        n (int): Number of nodes in the layer.
+        activation: Activation function to use.
+
+    Returns:
+        Tensor: Activated output of the new layer.
+    """
     initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
 
-    dense = tf.keras.layers.Dense(
-        n,
-        use_bias=False,
-        kernel_initializer=initializer
+    Z = tf.keras.layers.Dense(
+        units=n,
+        kernel_initializer=initializer,
+        use_bias=False
     )(prev)
 
-    batch_norm = tf.keras.layers.BatchNormalization(
-        epsilon=1e-7,
-        beta_initializer='zeros',
-        gamma_initializer='ones'
-    )(dense)
+    mean, variance = tf.nn.moments(Z, axes=[0])
 
-    return activation(batch_norm)
+    gamma = tf.Variable(tf.ones([n]), trainable=True)
+    beta = tf.Variable(tf.zeros([n]), trainable=True)
+
+    Z_norm = tf.nn.batch_normalization(
+        Z,
+        mean,
+        variance,
+        offset=beta,
+        scale=gamma,
+        variance_epsilon=1e-7
+    )
+
+    return activation(Z_norm)
