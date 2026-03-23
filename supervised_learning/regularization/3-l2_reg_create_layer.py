b742c59f14bcacde82cd4f5e276b995e73032537
acepilot950-creator <acepilot950@gmail.com>
2026-03-24 01:31:27 +0400

    initial commit

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -5,10 +5,15 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """creates a tensorflow layer with L2 regularization"""
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode="fan_avg"
+    )
 
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
+        kernel_initializer=initializer,
         kernel_regularizer=tf.keras.regularizers.l2(lambtha)
     )
 
