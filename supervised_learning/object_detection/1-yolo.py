013abe5952948b5eec6b11f82482619b60d90af0
kamranverdizade <acepilot950@gmail.com>
2026-05-03 20:28:58 +0400

    Update 1-yolo.py

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -6,10 +6,10 @@ import tensorflow.keras as K
 
 
 class Yolo:
-    """Uses the YOLO v3 algorithm to perform object detection."""
+    """YOLO v3 object detection"""
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        """Initialize Yolo."""
+        """Initialize"""
         self.model = K.models.load_model(model_path)
         self.class_names = self._load_classes(classes_path)
         self.class_t = class_t
@@ -17,19 +17,22 @@ class Yolo:
         self.anchors = anchors
 
     def _load_classes(self, classes_path):
-        """Load class names."""
+        """Load classes"""
         with open(classes_path, 'r') as f:
             return f.read().splitlines()
 
     def sigmoid(self, x):
-        """Sigmoid function."""
+        """Sigmoid"""
         return 1 / (1 + np.exp(-x))
 
     def process_outputs(self, outputs, image_size):
-        """Process outputs."""
+        """Process outputs"""
         boxes = []
         box_confidences = []
         box_class_probs = []
+
+        input_h = self.model.input.shape[1]
+        input_w = self.model.input.shape[2]
 
         image_h, image_w = image_size
 
@@ -44,7 +47,7 @@ class Yolo:
             confidence = self.sigmoid(output[..., 4:5])
             class_probs = self.sigmoid(output[..., 5:])
 
-            # Grid
+            # grid
             cx = np.arange(grid_w)
             cy = np.arange(grid_h)
             cx, cy = np.meshgrid(cx, cy)
@@ -52,19 +55,19 @@ class Yolo:
             cx = np.expand_dims(cx, axis=2)
             cy = np.expand_dims(cy, axis=2)
 
-            # bx, by (НЕ делим!)
+            
             bx = self.sigmoid(tx) + cx
             by = self.sigmoid(ty) + cy
 
-            # Anchors
+            # anchors
             anchor_w = self.anchors[i, :, 0].reshape((1, 1, anchor_boxes))
             anchor_h = self.anchors[i, :, 1].reshape((1, 1, anchor_boxes))
 
             
-            bw = anchor_w * np.exp(tw)
-            bh = anchor_h * np.exp(th)
+            bw = (anchor_w * np.exp(tw)) / input_w
+            bh = (anchor_h * np.exp(th)) / input_h
 
-           
+            # масштаб
             x1 = (bx - bw / 2) * (image_w / grid_w)
             y1 = (by - bh / 2) * (image_h / grid_h)
             x2 = (bx + bw / 2) * (image_w / grid_w)
