ae6b07805b166cf01c56f1753910d89c779676be
acepilot950-creator <acepilot950@gmail.com>
2026-05-17 23:53:48 +0400

    initial commit

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
-"""Performs Principal Component Analysis."""
+"""PCA module."""
 
 import numpy as np
 
 
 def pca(X, var=0.95):
-    """Performs PCA on a dataset.
-
-    Args:
-        X: Centered dataset of shape (n, d).
-        var: Fraction of variance to maintain.
-
-    Returns:
-        Weights matrix of shape (d, nd).
-    """
+    """Perform PCA on a centered dataset."""
     U, S, Vt = np.linalg.svd(X)
 
-    variance = S ** 2
-    cumulative = np.cumsum(variance) / np.sum(variance)
+    s = np.cumsum(S ** 2)
+    total = np.sum(S ** 2)
 
-    nd = np.argmax(cumulative >= var) + 1
+    nd = np.sum(s / total < var) + 1
 
     W = Vt[:nd].T
 
