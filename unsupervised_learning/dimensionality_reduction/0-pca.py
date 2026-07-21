4a81188d2e8a91b93eb58c1a897015913eb48999
kamranverdizade <acepilot950@gmail.com>
2026-07-21 23:49:14 +0400

    Update 0-pca.py

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python3
-"""PCA module."""
+"""Principal component analysis module."""
 
 import numpy as np
 
 
 def pca(X, var=0.95):
-    """Perform PCA while maintaining a specified fraction of variance.
+    """Perform PCA while maintaining a fraction of data variance.
 
     Args:
         X: A numpy.ndarray of shape (n, d) containing centered data.
-        var: The fraction of the original variance to maintain.
+        var: The fraction of variance that should be maintained.
 
     Returns:
         A numpy.ndarray of shape (d, nd) containing the weights matrix.
     """
-    covariance = np.cov(X, rowvar=False)
-    eigenvalues, eigenvectors = np.linalg.eigh(covariance)
+    _, singular_values, vh = np.linalg.svd(X, full_matrices=False)
 
-    indexes = np.argsort(eigenvalues)[::-1]
-    eigenvalues = eigenvalues[indexes]
-    eigenvectors = eigenvectors[:, indexes]
-
-    cumulative = np.cumsum(eigenvalues)
-    cumulative /= np.sum(eigenvalues)
+    cumulative = np.cumsum(singular_values)
+    cumulative /= np.sum(singular_values)
 
     nd = np.searchsorted(cumulative, var) + 1
 
-    return eigenvectors[:, :nd]
+    return vh[:nd].T
