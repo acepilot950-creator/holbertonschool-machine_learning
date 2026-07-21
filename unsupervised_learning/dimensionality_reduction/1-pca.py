4ea9a9c5ed652676eaefb7b3d4f3b04d3ba8cd98
kamranverdizade <acepilot950@gmail.com>
2026-07-21 23:52:12 +0400

    Update 1-pca.py

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python3
+"""Module for performing principal component analysis."""
 
+import numpy as np
+
+
+def pca(X, ndim):
+    """Transform a dataset using principal component analysis.
+
+    Args:
+        X: A numpy.ndarray of shape (n, d) containing the dataset.
+        ndim: The number of dimensions in the transformed dataset.
+
+    Returns:
+        A numpy.ndarray of shape (n, ndim) containing the transformed
+        dataset.
+    """
+    X_centered = X - np.mean(X, axis=0)
+
+    _, _, vh = np.linalg.svd(X_centered, full_matrices=False)
+
+    W = vh[:ndim].T
+
+    return np.matmul(X_centered, W)
