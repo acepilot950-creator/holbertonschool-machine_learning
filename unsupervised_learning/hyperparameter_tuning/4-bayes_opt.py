1402dccebd1c51822a79d75024fd35040093add8
kamranverdizade <acepilot950@gmail.com>
2026-07-23 01:01:02 +0400

    Update 4-bayes_opt.py

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -8,7 +8,7 @@ GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
-    """Performs Bayesian optimization on a noiseless 1D Gaussian process."""
+    """Perform Bayesian optimization on a noiseless 1D Gaussian process."""
 
     def __init__(self, f, X_init, Y_init, bounds, ac_samples,
                  l=1, sigma_f=1, xsi=0.01, minimize=True):
@@ -19,9 +19,9 @@ class BayesianOptimization:
             f: Black-box function to optimize.
             X_init: Initial input samples of shape (t, 1).
             Y_init: Initial output samples of shape (t, 1).
-            bounds: Tuple containing minimum and maximum search bounds.
+            bounds: Minimum and maximum search bounds.
             ac_samples: Number of acquisition sample points.
-            l: Length parameter of the Gaussian Process kernel.
+            l: Length parameter of the kernel.
             sigma_f: Standard deviation of the function output.
             xsi: Exploration-exploitation factor.
             minimize: Whether to minimize or maximize the function.
@@ -29,9 +29,7 @@ class BayesianOptimization:
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
         self.X_s = np.linspace(
-            bounds[0],
-            bounds[1],
-            ac_samples
+            bounds[0], bounds[1], ac_samples
         ).reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
@@ -39,8 +37,6 @@ class BayesianOptimization:
     def acquisition(self):
         """
         Calculate the next best sample location.
-
-        Uses the Expected Improvement acquisition function.
 
         Returns:
             X_next: Next best sample point of shape (1,).
@@ -57,8 +53,7 @@ class BayesianOptimization:
 
         with np.errstate(divide='ignore', invalid='ignore'):
             Z = improvement / sigma
-            EI = (improvement * norm.cdf(Z) +
-                  sigma * norm.pdf(Z))
+            EI = improvement * norm.cdf(Z) + sigma * norm.pdf(Z)
 
         EI[sigma == 0] = 0
         X_next = self.X_s[np.argmax(EI)]
