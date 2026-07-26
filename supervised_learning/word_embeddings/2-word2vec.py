66f28280d6199a1cd1990ef7c3c2305c8f168868
kamranverdizade <acepilot950@gmail.com>
2026-07-26 19:00:44 +0400

    Update 2-word2vec.py

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -8,7 +8,6 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """Create, build, and train a Gensim Word2Vec model."""
     model = gensim.models.Word2Vec(
-        sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
         window=window,
@@ -17,6 +16,8 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         seed=seed,
         workers=workers
     )
+
+    model.build_vocab(sentences)
 
     model.train(
         sentences,
