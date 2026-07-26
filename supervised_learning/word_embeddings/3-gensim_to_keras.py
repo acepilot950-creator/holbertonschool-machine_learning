76a2ca4edf497ef2572a8b38ee80ac22cd5705e1
kamranverdizade <acepilot950@gmail.com>
2026-07-26 19:57:21 +0400

    Update 3-gensim_to_keras.py

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -5,12 +5,31 @@ import tensorflow as tf
 
 
 def gensim_to_keras(model):
-    """Convert a Word2Vec model to a trainable Keras embedding."""
-    return tf.keras.layers.Embedding(
-        input_dim=len(model.wv.index_to_key),
+    """Convert a Gensim Word2Vec model to a trainable Keras layer.
+
+    Args:
+        model: A trained Gensim Word2Vec model.
+
+    Returns:
+        A trainable Keras Embedding layer initialized with the
+        Word2Vec vectors.
+    """
+    words = model.wv.index_to_key
+
+    weights = [
+        model.wv.get_vector(word)
+        for word in words
+    ]
+
+    weights = tf.stack(weights)
+
+    layer = tf.keras.layers.Embedding(
+        input_dim=len(words),
         output_dim=model.wv.vector_size,
-        embeddings_initializer=tf.keras.initializers.Constant(
-            model.wv.vectors
-        ),
+        embeddings_initializer=tf.keras.initializers.Constant(weights),
         trainable=True
     )
+
+    layer.build((None,))
+
+    return layer
