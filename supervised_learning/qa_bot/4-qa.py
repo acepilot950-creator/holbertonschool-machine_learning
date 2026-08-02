b1305bea4adadb68c26258e09fb3f058a511ffa6
acepilot950-creator <acepilot950@gmail.com>
2026-08-02 21:03:30 +0400

    intial commit

---
+++ supervised_learning/qa_bot/4-qa.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Module for question answering across multiple reference documents."""
+
+semantic_search = __import__(
+    '3-semantic_search'
+).semantic_search
+
+find_answer = __import__('0-qa').question_answer
+
+
+def question_answer(corpus_path):
+    """Answer questions using documents from a reference corpus.
+
+    Args:
+        corpus_path (str): Path to the directory containing reference
+            documents.
+    """
+    exit_words = {"exit", "quit", "goodbye", "bye"}
+
+    while True:
+        question = input("Q: ")
+
+        if question.lower() in exit_words:
+            print("A: Goodbye")
+            break
+
+        reference = semantic_search(corpus_path, question)
+        answer = find_answer(question, reference)
+
+        if answer is None:
+            print("A: Sorry, I do not understand your question.")
+        else:
+            print("A: {}".format(answer))
