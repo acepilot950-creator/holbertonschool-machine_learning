7ba2ddc8d4b39a38caa6104bf449256fc7a5f90c
acepilot950-creator <acepilot950@gmail.com>
2026-08-31 23:17:21 +0400

    initial commit

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -9,3 +9,16 @@ def policy(matrix, weight):
     z = matrix @ weight
     exp = np.exp(z)
     return exp / np.sum(exp, axis=1, keepdims=True)
+
+
+def policy_gradient(state, weight):
+    """Compute the Monte-Carlo policy gradient."""
+    probabilities = policy(state.reshape(1, -1), weight)[0]
+    action = np.random.choice(len(probabilities), p=probabilities)
+
+    one_hot = np.zeros_like(probabilities)
+    one_hot[action] = 1
+
+    gradient = np.outer(state, one_hot - probabilities)
+
+    return action, gradient
