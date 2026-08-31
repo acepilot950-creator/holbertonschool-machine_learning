6c8a6a5cd430e2ab8f307ce9b936c0eaec8a5b8f
acepilot950-creator <acepilot950@gmail.com>
2026-08-31 23:20:48 +0400

    initial commit

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -6,7 +6,8 @@ import numpy as np
 policy_gradient = __import__('policy_gradient').policy_gradient
 
 
-def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98,
+          show_result=False):
     """Train an agent using the Monte-Carlo policy gradient method."""
     weight = np.random.rand(
         env.observation_space.shape[0],
@@ -24,6 +25,9 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
         truncated = False
 
         while not (terminated or truncated):
+            if show_result and episode % 1000 == 0:
+                env.render()
+
             action, gradient = policy_gradient(state, weight)
 
             state, reward, terminated, truncated, _ = env.step(action)
